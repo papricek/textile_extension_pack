dba59ae536483be29bd3737e90d534aa4a0d29db
Patrik Jira <patrikjira@gmail.com>
2012-03-21 10:22:24 +0100

    Fix and raising

--- lib/textile_extension_pack/tags.rb
+++ lib/textile_extension_pack/tags.rb
@@ -16,6 +16,26 @@ module TextileExtensionPack
       html << %Q{  <figcaption>#{text}</figcaption>\n} if text.present?
       html << %Q{</figure>\n}
     end
+
+    def video(opts)
+      file_name, text, img = opts[:text].split('|').map! { |str| str.strip }
+      html = ""
+      html << %Q{<div class="video-wrapper #{opts[:class]}">\n}
+      html << %Q{<div class="video-wrapper-inner">\n}
+      html << %Q{<div class="video-wrapper-inner-inner">\n}
+      html << %Q{ <video width="700" height="395" id="player#{file_name}" poster="#{img}" controls="controls" preload="none"> }
+      {:mp4 => "mp4", :webm => "webm", :ogg => "ogv"}.each do |format, extension|
+        html << %Q{ <source type="video/#{format}" src="/files/videos/#{file_name}.#{extension}"/> }
+      end
+      html << %Q{ <object width="700" height="395" type="application/x-shockwave-flash" data="/mediaelements/flashmediaelement.swf">
+                <param name="movie" value="/mediaelements/flashmediaelement.swf"/>
+                <param name="flashvars" value="controls=true&amp;file=/files/videos/#{file_name}.mp4"/>
+              </object> }
+      html << %Q{  <p>#{text}</p>\n} if text.present?
+      html << %Q{</div>\n}
+      html << %Q{</div>\n}
+      html << %Q{</div>\n}
+    end
   end
 
 end
